6f06ee288c25fe7a377243512cbb00c6b5d4b909
uv-07 <63532070+uv-07@users.noreply.github.com>
2023-02-19 19:41:45 +0530

    routes/fix

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
-app.use("api/v1/post", postRoutes);
-app.use('api/v1/dalle', dalleRoutes);
+app.use("/api/v1/post", postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Server running");
